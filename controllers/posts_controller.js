07cc92926df324de3a6f351946de255ad071fd5a
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-03-17 00:45:36 +0100

    enhance show and delete & add id to data & add 404 error (milestone 2 & bonbus 2)

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,13 +2,23 @@ const postsData = require("../data/posts_data")
 
 /* index */
 function index(req, res) {
-    res.json(postsData)
+    res.json(postsData);
 }
 
 /* show */
 function show(req, res) {
-    res.send(`Showing post n°${req.params.id}`)
+    const id = parseInt(req.params.id)
+    const thisPost = postsData.find(thisPost => thisPost.id === id);
+
+    if (!thisPost) {
+        res.status(404).send(`Error 404: Post not found, we currently have ${postsData.length} posts, try again with a valid number`);
+    } else {
+        res.json(thisPost);
+    }
 }
+
+/* res.send(`Showing post n°` + req.params.id + postsData[req.params.id - 1]) */
+/* res.send('Dettagli della pizza ' + req.params.id); */
 
 /* create */
 function create(req, res) {
@@ -27,7 +37,18 @@ function modify(req, res) {
 
 /* delete */
 function destroy(req, res) {
-    res.send(`Delete post n°${req.params.id}`)
+    const id = parseInt(req.params.id);
+    const thisPostIndex = postsData.findIndex(post => post.id === id);
+
+    if (thisPostIndex === -1) {
+        return res.status(404).send(`Error 404: Post not found, we currently have ${postsData.length} posts, please try again with a valid number`);
+    } else {
+        postsData.splice(thisPostIndex, 1);
+
+        console.log("Updated posts list:", postsData);
+
+        res.status(204).send();
+    }
 }
 
 module.exports = { index, show, create, update, modify, destroy };
